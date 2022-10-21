85e0dcd755a4f6a779d30a8196b75e02f82a9030
Jeffrey Jose <jefjosecool@gmail.com>
2022-10-20 23:44:01 -0400

    Did exercise 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+
+
+//----------------------Exercise 1------------------
+
+for (int i=0;i<15;i++)
+{
+
+    Console.WriteLine($"{i}");
+
+}
